746c4afb2377856fcea120bb6b8af1caa73a5641
Soulgypsy79 <jackbates065@gfmailc.om>
2023-03-08 15:24:07 +0000

    broken check point system for agent functionality, plz fix if I can't get to it :(

--- Assets/Scripts/Quest Scripts/CheckpointScript.cs
+++ Assets/Scripts/Quest Scripts/CheckpointScript.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CheckpointScript : MonoBehaviour
 {
     private CheckpointArray checkpointArray;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        //Hide();
+    }
+
+    private void Update()
+    {
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,10 +29,24 @@ public class CheckpointScript : MonoBehaviour
             checkpointArray.PlayerThroughCheckpointTransform(this);
             gameObject.SetActive(false);
         }
+
+        if (other.tag == "RaceAgent")
+        {
+            checkpointArray.AgentThroughCheckpointTransform(this);
+        }
     }
 
     public void SetTrackCheckpoint(CheckpointArray checkpointArray)
     {
         this.checkpointArray = checkpointArray;
+    }
+
+    public void Hide()
+    {
+        meshRenderer.enabled = false;
+    }
+    public void Show()
+    {
+        meshRenderer.enabled = true;
     }
 }
