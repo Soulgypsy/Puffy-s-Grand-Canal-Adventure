1a658c84e10bcabb756dd2815c6d7635214cf8c9
Gabriel-Harvey <77337983+Gabriel-Harvey@users.noreply.github.com>
2023-03-16 10:34:46 +0000

    Level 1 Functionality

---
+++ Assets/Scripts/Level 1/TutorialTriggers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriggers : MonoBehaviour
+{
+    public BoatTutorial boatTutorial;
+    public bool isHarpoonTrigger;
+    public bool isExitTrigger;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (isHarpoonTrigger)
+        {
+            gameObject.SetActive(false);
+            boatTutorial.equipHarpoon();
+        }
+        else if (isExitTrigger)
+        {
+            boatTutorial.exitTrigger();
+        }
+        else
+        {
+            if (other.tag == "PlayerBoat")
+            {
+                boatTutorial.partSelect();
+            }
+        }
+        
+    }
+}
